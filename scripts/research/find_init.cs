ab7b479213b39e8460f51534f6d984723c0659c7
lennix1337 <swlucas05@gmail.com>
2026-02-20 17:05:14 -0300

    chore: move research scripts to scripts/research

---
+++ scripts/research/find_init.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+using System.Linq;
+
+public class Finder {
+    public static void Main() {
+        string gxPath = @"C:\Program Files (x86)\GeneXus\GeneXus18";
+        var dlls = Directory.GetFiles(gxPath, "Artech.*.dll");
+        foreach (var dll in dlls) {
+            try {
+                var asm = Assembly.LoadFrom(dll);
+                foreach (var type in asm.GetTypes()) {
+                    try {
+                        var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Static);
+                        foreach (var method in methods) {
+                            if ((method.Name == "Initialize" || method.Name == "Init") && method.GetParameters().Length == 0) {
+                                Console.WriteLine($"{Path.GetFileName(dll)}: {type.FullName}.{method.Name}");
+                            }
+                        }
+                    } catch {}
+                }
+            } catch {}
+        }
+    }
+}
+
---
+++ scripts/research/find_types.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+
+public class Finder {
+    public static void Main() {
+        string gxPath = @'C:\Program Files (x86)\GeneXus\GeneXus18';
+        string[] dlls = Directory.GetFiles(gxPath, 'Artech.*.dll');
+        foreach (var dll in dlls) {
+            try {
+                var asm = Assembly.LoadFrom(dll);
+                foreach (var type in asm.GetTypes()) {
+                    if (type.Name == 'ContextService') {
+                        Console.WriteLine($'Found ContextService in {Path.GetFileName(dll)}: {type.FullName}');
+                    }
+                    if (type.Name == 'UIServices') {
+                        Console.WriteLine($'Found UIServices in {Path.GetFileName(dll)}: {type.FullName}');
+                    }
+                }
+            } catch {}
+        }
+    }
+}
